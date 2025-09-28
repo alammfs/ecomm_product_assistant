24e475cff828600fbf160630677f50974b250d57
alam <malam1984@gmail.com>
2025-09-28 15:31:13 +0530

    code is sync with mentor

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -4,6 +4,7 @@ import json
 from dotenv import load_dotenv
 from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ProductAssistantException
@@ -11,6 +12,7 @@ import asyncio
 
 
 class ApiKeyManager:
+    # REQUIRED_KEYS = ["GROQ_API_KEY", "GOOGLE_API_KEY", "OPENAI_API_KEY"]
     REQUIRED_KEYS = ["GROQ_API_KEY", "GOOGLE_API_KEY"]
 
     def __init__(self):
@@ -126,13 +128,12 @@ class ModelLoader:
                 temperature=temperature,
             )
 
-        # elif provider == "openai":
-        #     return ChatOpenAI(
-        #         model=model_name,
-        #         api_key=self.api_key_mgr.get("OPENAI_API_KEY"),
-        #         temperature=temperature,
-        #         max_tokens=max_tokens
-        #     )
+        elif provider == "openai":
+            return ChatOpenAI(
+                model=model_name,
+                api_key=self.api_key_mgr.get("OPENAI_API_KEY"),
+                temperature=temperature
+            )
 
         else:
             log.error("Unsupported LLM provider", provider=provider)
